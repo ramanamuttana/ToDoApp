a120e0ec6372a94e51117d37dad78d9139e76080
ramanamuttana <ramanamuttana@gmail.com>
2024-05-25 16:24:21 +0200

    added the code for video31

---
+++ video31/ToDoApp/src/main/java/com/example/demo/controller/Sayhellocontroller.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Sayhellocontroller {
+
+
+	//say-hello
+	// http://localhost:8080/say-hello
+	@RequestMapping("say-hello")
+	@ResponseBody
+	public String sayHello() {
+		return "Hello! What are you learning today?";
+	}
+
+	@RequestMapping("say-hello-html")
+	@ResponseBody
+	public String sayHelloHtml() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("<html>");
+		sb.append("<head>");
+		sb.append("<title> My First HTML Page - Changed</title>");
+		sb.append("</head>");
+		sb.append("<body>");
+		sb.append("My first html page with body - Changed");
+		sb.append("</body>");
+		sb.append("</html>");
+
+		return sb.toString();
+	}
+}
